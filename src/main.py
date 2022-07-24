c46d7f9fc933eb592e884f230aac745acb7e704a
MrOakNextDoor <keoknowsis@gmail.com>
2022-07-24 22:52:56 +0800

    Added kick, ban, and censorlist checking

--- src/main.py
+++ src/main.py
@@ -16,7 +16,11 @@ cursor = conn.cursor()
 
 #   Initialize the database if possible
 try:
-    cursor.execute()
+    cursor.execute("""CREATE TABLE censorlist
+        (usr_id integer);
+    """)
+
+    conn.commit()
 except sqlite3.OperationalError:
     pass
 
@@ -30,18 +34,46 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    await client.process_commands(message)
-
     if message.author == client.user:
         return
+
+    #   Check if the sender is part of the censorlist
+    cursor.execute('SELECT * FROM censorlist WHERE usr_id == ?', [message.author.id])
+    if not cursor.fetchall() == []:
+        await message.channel.send(
+            f'You may not speak, <@{message.author.id}>.')
+        return
+
+    #   Then process all commands
+    await client.process_commands(message)
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'My ping is {round(client.latency * 1000, 1)} ms!')
 
 @client.command()
-async def clear(ctx, num=5):
+async def clear(ctx, num: int=5):
     await ctx.channel.purge(limit=num+1)
+
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason: str=None):
+    await member.kick(reason=reason)
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason: str=None):
+    await member.ban(reason=reason)
+
+@kick.error
+async def kick_error(error, ctx):
+    if isinstance(error, commands.MissingPermissions):
+       await ctx.send(f'You don\'t have permission to do that, <@{ctx.author.id}>.')
+
+@ban.error
+async def ban_error(error, ctx):
+    if isinstance(error, commands.MissingPermissions):
+       await ctx.send(f'You don\'t have permission to do that, <@{ctx.author.id}>.')
 
 if __name__ == '__main__':
     #   Run the bot
